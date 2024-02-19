d69511cc9fca063b859c3afa50e393dc8911db2f
denfry <daniilyurkov16@gmail.com>
2024-02-19 22:41:23 +0300

    Добавьте файлы проекта.

---
+++ Lab2TP/InputForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2TP
+{
+    public partial class InputForm : Form
+    {
+        public InputForm()
+        {
+            InitializeComponent();
+        }
+        public int GetBaseLength()
+        {
+            if (int.TryParse(textBox1.Text, out int baseLength))
+                return baseLength;
+            else
+                return -1;
+        }
+
+        public int GetSideLength()
+        {
+            if (int.TryParse(textBox2.Text, out int sideLength))
+                return sideLength;
+            else
+                return -1;
+        }
+
+
+
+        private void ContinueButton(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Введите значения для длины основания и длины стороны треугольника.");
+            }
+            else
+            {
+                if (!int.TryParse(textBox1.Text, out int baseLength) || !int.TryParse(textBox2.Text, out int sideLength))
+                {
+                    MessageBox.Show("Значения должны быть целыми числами.");
+                    return;
+                }
+
+                if (baseLength <= 0 || sideLength <= 0)
+                {
+                    MessageBox.Show("Значения должны быть положительными.");
+                    return;
+                }
+
+                if (baseLength >= sideLength + sideLength || sideLength >= baseLength + baseLength)
+                {
+                    MessageBox.Show("Значения не могут образовать стороны треугольника.");
+                    return;
+                }
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+
+        private void CancelButton(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
---
+++ Lab2TP/MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2TP
+{
+    public partial class MainForm : Form
+
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        isoscelesTriangle triangle1 = new isoscelesTriangle(10, 12);
+       
+        private void InputButton(object sender, EventArgs e)
+        {
+            InputForm inputForm = new InputForm();
+            
+            DialogResult result = inputForm.ShowDialog();
+
+            
+            if (result == DialogResult.OK)
+            {
+                
+                int baseLength = inputForm.GetBaseLength();
+                int sideLength = inputForm.GetSideLength();
+
+                
+                triangle1 = new isoscelesTriangle(baseLength, sideLength);
+            }
+        }
+        private void OutputButton(object sender, EventArgs e)
+        {
+            if (triangle1 != null)
+            {
+                
+                ResultForm resultForm = new ResultForm();
+
+                
+                int perimeter = triangle1.calcutePerimetr();
+                int square = triangle1.calcuteSquare();
+
+                
+                resultForm.SetResults(perimeter, square);
+
+                resultForm.ShowDialog();
+            }
+        }
+        private void calculateAreaNPerimetr(object sender, EventArgs e)
+        {
+            
+            if (!string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                
+                if (int.TryParse(textBox1.Text, out int baseLength) && int.TryParse(textBox2.Text, out int sideLength))
+                {
+                    
+                    if (baseLength > 0 && sideLength > 0)
+                    {
+                        if (baseLength < sideLength + sideLength && sideLength < baseLength + sideLength)
+                        {
+                            isoscelesTriangle triangle = new isoscelesTriangle(baseLength, sideLength);
+
+                            ResultForm resultForm = new ResultForm();
+                            resultForm.SetResults(triangle.calcutePerimetr(), triangle.calcuteSquare());
+                            resultForm.ShowDialog();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Стороны должны соответсвовать правилу треугольника");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введите положительные значения для длины основания и длины стороны");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Введите числовые значения для длины основания и длины стороны");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите значения для длины основания и длины стороны");
+            }
+        }
+        /// <summary>
+        /// Ru: Класс для расчёта периметра и площади равнобедренного треугольника.<br/>
+        /// En: A class for calculating the perimeter and area of an isosceles triangle.
+        /// </summary>
+
+        class isoscelesTriangle
+        {
+            private int baseLength;
+            private int sideLength;
+
+            public isoscelesTriangle(int baseLength, int sideLength)
+            {
+                this.baseLength = baseLength;
+                this.sideLength = sideLength;
+            }
+
+            public int calcutePerimetr()
+            {
+                return baseLength + 2 * sideLength;
+            }
+
+            public int calcuteSquare()
+            {
+                return baseLength * sideLength / 2;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+            triangle1 = new isoscelesTriangle(10, 12);
+        }
+
+      
+    }
+}
